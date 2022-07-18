4def842224ac44fe5ea241c7aff5337fde48f6fc
nayeemnishaat <rxnayeemimi@gmail.com>
2022-07-19 01:41:50 +0600

    implemented server

--- server.js
+++ server.js
@@ -2,34 +2,35 @@ const http = require("node:http");
 
 const server = http.createServer((req, res) => {
   let rawData = "";
+
   req.on("data", (chunk) => {
-    console.log("sd", chunk);
     rawData += chunk;
   });
+
   req.on("end", () => {
     try {
       const parsedData = JSON.parse(rawData);
       console.log(parsedData);
+
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+      res.setHeader("Access-Control-Max-Age", "3600");
+      res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+      res.end(
+        JSON.stringify({
+          status: "success"
+        })
+      );
     } catch (e) {
       console.error(e.message);
+
+      res.statusCode = 500;
+      res.end(JSON.stringify({ status: "fail" }));
     }
   });
-
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
-  res.setHeader("Access-Control-Max-Age", "3600");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
-  );
-
-  res.end(
-    JSON.stringify({
-      data: "Hello World!"
-    })
-  );
 });
 
 server.listen(5000, () => {
-  console.log("Server started at port 5000");
+  console.log("Server listening on port 5000.");
 });
